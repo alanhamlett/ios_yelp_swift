732e258e50228bef1cf3bbfedcbccca11a54f305
Alan Hamlett <alan.hamlett@airbnb.com>
2017-04-03 10:28:53 -0700

    hookup api data to table view cells

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,47 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by alan_hamlett on 4/3/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbView: UIImageView!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var revewsCountLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbView.setImageWith(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            revewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+
+        thumbView.layer.cornerRadius = 5
+        thumbView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
